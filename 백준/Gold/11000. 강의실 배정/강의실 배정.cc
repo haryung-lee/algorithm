c3600c87bc4455b7f95e54e2fc10c14baf052066
halang <lhr4884@naver.com>
2023-01-23 18:56:49 +0900

    [Gold V] Title: 강의실 배정, Time: 136 ms, Memory: 11396 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,24 +1,15 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
 #include <algorithm>
-#include <cmath>
 #include <queue>
 #include <deque>
-#include <map>
-#include <unordered_map>
+#include <set>
 using namespace std;
 typedef pair<int, int> pii;
-typedef pair<int, char> pic;
-typedef long long ll;
-int dy[] = {0, 1, 0, -1}, dx[] = {-1, 0, 1, 0};
-struct axis { int x, y; };
 typedef pair<pii, int> piii;
 
 int n;
-pii arr[200000];
-map<pii, int> mp;
+multiset<pii> mp;
 
 int main() {
     int ans = 0;
@@ -26,22 +17,18 @@ int main() {
     for(int i = 0; i < n; i++) {
         int a, b;
         scanf("%d %d", &a, &b);
-        mp[{a, b}]++;
+        mp.insert({a, b});
     }
 
     while(!mp.empty()) {
         auto it = mp.begin(); ans++;
-        piii itValue = *mp.begin();
-//        printf("1 : %d %d %d\n", itValue.first.first, itValue.first.second, itValue.second);
-        if (itValue.second > 1) mp[itValue.first]--;
-        else mp.erase(it);
-        while(mp.lower_bound({itValue.first.second, -1}) != mp.end()) {
-            it = mp.lower_bound({itValue.first.second, -1});
-            itValue = *it;
+        pii itValue = *mp.begin();
 
-            if (itValue.second > 1) mp[itValue.first]--;
-            else mp.erase(it);
-//            printf("2 : %d %d %d\n", itValue.first.first, itValue.first.second, itValue.second);
+        mp.erase(it);
+        while(mp.lower_bound({itValue.second, -1}) != mp.end()) {
+            it = mp.lower_bound({itValue.second, -1});
+            itValue = *it;
+            mp.erase(it);
         }
     }
     printf("%d", ans);
