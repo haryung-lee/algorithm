3ba6b2b529505a3a275e0ec45c26048f4ec2c2d5
halang <lhr4884@naver.com>
2023-03-19 22:54:10 +0900

    [Silver I] Title: 접두사 찾기, Time: 172 ms, Memory: 529364 KB -BaekjoonHub

---
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+typedef long long ll;
+int dy[] = {0, 1, 0, -1}, dx[] = {-1, 0, 1, 0};
+struct axis { int x, y; };
+typedef pair<string, int> psi;
+typedef pair<axis, psi> pas;
+
+int n, m;
+int trie[10000 * 500 + 1][26]; // [최대 문자열 개수 * 최대 길이 + 1][알파벳 소문자]
+int isVisit[10000*500+1];
+char s[501];
+int num = 1;
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) {
+        scanf("%s", s);
+
+        int now = 0;
+        for (int j = 0; s[j]; j++) { // 문자열 s만큼
+            char t = s[j] - 'a';
+            if (trie[now][t] == 0) trie[now][t] = num++;
+            now = trie[now][t];
+        }
+        isVisit[now] = 1;
+
+    }
+
+    int ans = 0;
+    for(int i = 0; i < m; i++) {
+        scanf("%s", s);
+
+        int now = 0, isFind = 1;
+        for(int j = 0; s[j]; j++) {
+            char t = s[j] - 'a';
+            if (trie[now][t] == 0) {
+                isFind = 0;
+                break;
+            }
+            now = trie[now][t];
+        }
+
+        ans += isFind;
+    }
+    printf("%d", ans);
+
+}
