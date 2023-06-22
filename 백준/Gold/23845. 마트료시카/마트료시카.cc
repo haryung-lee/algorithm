0eae1097a594bce6e6c0d10af873552a53e41cdd
halang <lhr4884@naver.com>
2023-06-23 00:57:43 +0900

    [Gold III] Title: 마트료시카, Time: 48 ms, Memory: 3200 KB -BaekjoonHub

--- 백준/Gold/23845. 마트료시카/마트료시카.cc
+++ 백준/Gold/23845. 마트료시카/마트료시카.cc
@@ -1,47 +1,53 @@
 #include <iostream>
-#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include <algorithm>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
 using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+typedef long long ll;
+int dy[] = {0, 1, 0, -1}, dx[] = {-1, 0, 1, 0};
+struct axis { int x, y; };
+typedef pair<string, int> psi;
+typedef pair<axis, psi> pas;
+typedef pair<ll, ll> pll;
 
-int dollcount[100001];
-int main()
-{
-	long long N, Q = 0, cnt = 0;
-	long long answer = 0;
-	cin >> N;
-	vector<int> matryoshka(N);
-	for (int i = 0; i < N; i++)
-	{
-		cin >> matryoshka[i];
-		dollcount[matryoshka[i]]++;
-	}
-	sort(matryoshka.begin(), matryoshka.end());
+ll n;
+int cnt[100001];
+ll mx = 0, ans = 0;
 
-	for (int i = 0; i < N; i++)
-	{
-		if (dollcount[matryoshka[i]] >= 1)
-		{
-			dollcount[matryoshka[i]]--;
-			Q = matryoshka[i];
-			cnt = 1;
-			for (int j = matryoshka[i] + 1;; j++)
-			{
-				if (dollcount[j] >= 1)
-				{
-					Q = j;
-					dollcount[j]--;
-					cnt++;
-				}
-				else
-				{
-					answer += Q * cnt;
-					break;
-				}
-			}
-		}
-	}
-	
-	cout << answer;
 
-	return 0;
+int main() {
+    scanf("%lld", &n);
+    vector<int> doll(n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &doll[i]);
+        cnt[doll[i]]++;
+    }
+
+    sort(doll.begin(), doll.end());
+    for(int i = 0; i < n; i++) {
+        if (cnt[doll[i]] == 0) continue;
+        cnt[doll[i]]--;
+        mx = doll[i];
+        ll t = 1;
+        for(int j = doll[i]+1;; j++) {
+            if (cnt[j] <= 0) {
+                ans += mx * t;
+                break;
+            } else {
+                mx = j;
+                cnt[j]--;
+                t++;
+            }
+        }
+    }
+    printf("%lld\n", ans);
 }
