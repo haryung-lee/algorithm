c6b8d1b652d576078f770e4f70b2b494c75d530d
halang <lhr4884@naver.com>
2024-10-16 09:41:50 +0900

    [level 2] Title: 튜플, Time: 65.49 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  const answer = [];
+  const arr = s.slice(2, s.length - 2).split("},{");
+  arr.sort((a, b) => a.length - b.length);
+    
+  for (let i = 0; i < arr.length; i++) {
+    const temp = arr[i].split(",");
+    for (let j = 0; j < temp.length; j++) {
+      if (!answer.includes(Number(temp[j]))) {
+        answer.push(Number(temp[j]));
+      }
+    }
+  }
+  return answer;
+}
